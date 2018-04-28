a74dbb24b1cd1ff6c989185fa2afa344473ddb8c
Stormfluger <turkom76@mail.ru>
2018-04-28 11:19:39 +0500

    work

--- shop/js/cart.js
+++ shop/js/cart.js
@@ -1,1 +1,7 @@
+
 'use strict';
+
+list.addEventListener('click', event => {
+  event.preventDefault();
+  event.target.classList.contains('add-to-cart') ? addToCart({title: event.target.dataset.title, price: event.target.dataset.price}) : false;
+});
--- table/js/sort.js
+++ table/js/sort.js
@@ -1,5 +1,18 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+  const th = event.target,
+        table = event.currentTarget;
+
+  let thList = document.querySelectorAll('th');
+  thList = Array.from(thList).filter(item => {return item !== th});
+
+  Array.from(thList).forEach(item => {
+    item.removeAttribute('data-dir');
+  });
+
+  th.dataset.dir = th.dataset.dir ? -th.dataset.dir : 1;
+  sortTable(th.dataset.propName, th.dataset.dir);
+
+  table.dataset.sortBy = th.dataset.propName;
 }
