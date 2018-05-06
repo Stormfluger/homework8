d524c73af0b85d97cd44a5e67774f7d6aaec18c0
Stormfluger <turkom76@mail.ru>
2018-05-06 20:58:53 +0500

    change for optimal

--- table/js/sort.js
+++ table/js/sort.js
@@ -5,12 +5,13 @@ function handleTableClick(event) {
         table = event.currentTarget;
 
   let thList = document.querySelectorAll('th');
-  //thList = Array.from(thList).filter(item => {return item !== th});
-  Array.from(thList).forEach(item => {return item !== th});
-
-  Array.from(thList).forEach(item => {
-    item.removeAttribute('data-dir');
-  });
+ //first var
+  //thList = Array.from(thList).filter(item => {return item !== th}); 
+ //second var 
+  //Array.from(thList).forEach(item => {return item !== th});
+  //Array.from(thList).forEach(item => {item.removeAttribute('data-dir');});
+ //last var
+  Array.from(thList).forEach(item => { if (item !== th) {item.removeAttribute(`data-dir`);}});
 
   th.dataset.dir = th.dataset.dir ? -th.dataset.dir : 1;
   sortTable(th.dataset.propName, th.dataset.dir);
